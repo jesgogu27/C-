39fd7201cb86432958a7bb847d9230a8a589977a
jesgogu27 <jesgogu@hotmail.com>
2021-11-20 20:02:24 -0500

    cheers

---
+++ 97Ejercicios/1/Program.cs
@@ -0,0 +1,23 @@
+﻿// get a name and return a cheers
+
+using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Name");
+            print();
+            Console.WriteLine("Please, press any Key for finalice");
+            System.Console.ReadKey();
+        }
+        static void print()
+        {
+
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello:  {name}");
+        }
+    }
+}
