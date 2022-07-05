df5e69f4962d7ddc9e3a4247e6d55aa80568a709
jesgogu27 <jesgogu@hotmail.com>
2022-07-05 07:09:51 -0500

    Inventario

---
+++ MatrizAntiFraude/MatrizAntiFraude/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] fraudulentOrderIDs = {"A123","B456", "C789"};
+
+Console.WriteLine($"Firts:  {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third:  {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"There are {fraudulentOrderIDs.Length}"); 
