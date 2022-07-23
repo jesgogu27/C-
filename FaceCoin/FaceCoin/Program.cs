2c4d6873f32a6fe0a10d3351e0651c196fed7f52
jesgogu27 <jesgogu@hotmail.com>
2022-07-23 16:06:33 -0500

    Face of coin

---
+++ FaceCoin/FaceCoin/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+    Random ladoMoneda = new Random();
+    Console.WriteLine((ladoMoneda.Next(1, 3) == 1) ? "head" : "tail");
+ 
